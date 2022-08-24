71d74ba55e3d8cd79aa355abcb58098949906772
Tariquzzaman-faisal <tariquzzaman@iut-dhaka.edu>
2022-08-24 21:39:38 +0600

    navbar works in frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+// BrowserRouter aliased as Router to your app.js file in order to wrap all the other components
+// BrowserRouter is a parent component and can have only single child
+// To render a single component, wrap all the routes inside the Routes Component
+// Route component will now help us to establish the link between component’s UI and the URL
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' />  
+        </Routes>      
+      </Router>
     </div>
   );
 }
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react';
+import * as FaIcons from 'react-icons/fa'; //for fontawesome icons
+import * as AiIcons from 'react-icons/ai'; //for aiICons icons
+import { Link } from 'react-router-dom';
+// Anchor tags will reload the page and re-render all the components. While <Link> and <NavLink> 
+// will only re-render updated components matched with the URL path of the Route without reloading. 
+// It helps the Single-Page Applications to work faster while routing.
+import { sidebarData } from './SidebarData';
+import './Navbar.css'
+import {IconContext} from 'react-icons';
+
+function Navbar() {
+    
+    const [sidebar, setSidebar] = useState(false); // sidebar state initially set to false
+    const showSidebar = () =>setSidebar(!sidebar); // function to toggle the state of the sidebar
+    
+    return (
+    <div>
+        {/* Customize all the icons */}
+        <IconContext.Provider value={{color: '#fff'}}>
+            
+            {/* Navbar */}
+            <div className='navbar'>
+                <Link to="#" className="menu-bars"> 
+                    <FaIcons.FaBars onClick={showSidebar}/> 
+                    {/* top left bar icon, a clickable react component */}
+                </Link>
+            </div>
+
+            {/* Nav Menu */}
+            <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}> 
+            {/* checks whether Nav-Menu is active or not */}
+                <ul className='nav-menu-items' onClick={showSidebar}>
+                    <li className='navbar-toggle'>
+                        <Link to="#" className='menu-bars'>
+                            <AiIcons.AiOutlineClose />
+                        </Link>
+                    </li>
+                    {sidebarData.map((item, index) => {
+                        return (
+                            <li key={index} className={item.cName}>
+                                <Link to={item.path}>
+                                    {item.icon}
+                                    <span>{item.title}</span>
+                                </Link>
+                            </li>
+                        )
+                    })}
+                </ul>
+            
+            </nav>
+        </IconContext.Provider>
+    </div>
+  )
+}
+
+export default Navbar
