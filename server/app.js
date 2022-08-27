ad29933bb20d9c7f8d169ec1a5e432c3c278803e
samikhan-cse19 <samikhan@iut-dhaka.edu>
2022-08-27 15:52:36 +0600

    Added epub parsing and connected database

--- server/app.js
+++ server/app.js
@@ -1,13 +1,36 @@
 const Express = require("express");
-
+const EPub = require('epub2').EPub;
 const app = Express();
+
+const DbService = require('./DBService');
 
 app.use(Express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-    res.send("Hello");
+var results = DbService.getAllBooks();
+
+app.get("/books/data_communication_and_networking", (req, res) => {
+    
+    results.then((books) => {
+        EPub.createAsync("files/" + books[1]["title"])
+        .then(function (epub)
+        {
+            epub.getFile(epub.metadata.cover, (err, coverData, mimeType) => {
+                var json = epub.metadata;
+                json["cover"] = Buffer.from(coverData).toString('base64');
+                res.send(`<div><h3>`+json["title"]+`</h3><p>`+json["creator"]+`</p>
+                        <img src="data:${mimeType};base64,${json['cover']}"/></div>`);
+                // res.send(json);
+            });
+        })
+        .catch(function (err)
+        {
+            console.log("ERROR\n-----");
+            throw err;
+        });
+    })
+    
 });
 
 app.listen(3050, "localhost", () => {
-    console.log("Server is running on port 3050");
+    console.log("Server is running at http://localhost:3050");
 });
