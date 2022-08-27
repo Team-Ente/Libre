c2cf4ea9718b845cb14cdcc7647da6c2f0ffaca9
Tariquzzaman-faisal <tariquzzaman@iut-dhaka.edu>
2022-08-27 18:17:11 +0600

    Landing page almost complete

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,127 @@ import './App.css';
 import Logo from './Components/Logo/logo';
 import Header from './Components/header/header';
 import Sidenav from './Components/Sidenav/Sidenav';
+import Book from './Components/Book/Book';
+
 function App() {
   return (
     <div className='App'>
         <div className='container'>
           <div className='logo'><Logo /><Sidenav /></div>
           <div className='header'><Header /></div>
-          {/* <div className='nav'><Sidenav /></div> */}
-          <div className='content'>Content</div>
+          <div className='content'>
+            <h2 className='group'>Now Reading</h2>
+            <div className='books'>
+              <Book 
+              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
+              title='The Dictatorship Syndrome'
+              author='Alaa Al Aswany'
+              year='2019'
+              />
+              <Book 
+              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
+              title='Boikoter Jibon'
+              author='Sami Khan'
+              year='2022'
+              />
+              <Book 
+              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
+              title='Boikoter Jibon'
+              author='Sami Khan'
+              year='2022'
+              />
+              <Book 
+              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
+              title='Boikoter Jibon'
+              author='Sami Khan'
+              year='2022'
+              />
+            </div>
+
+            <h2 className='group'>Trending</h2>
+            <div className='books'>
+              <Book 
+              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
+              title='The Dictatorship Syndrome'
+              author='Alaa Al Aswany'
+              year='2019'
+              />
+              <Book 
+              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
+              title='Boikoter Jibon'
+              author='Sami Khan'
+              year='2022'
+              />
+              <Book 
+              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
+              title='Boikoter Jibon'
+              author='Sami Khan'
+              year='2022'
+              />
+              <Book 
+              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
+              title='Boikoter Jibon'
+              author='Sami Khan'
+              year='2022'
+              />
+            </div>
+
+            <h2 className='group'>Recently Added</h2>
+            <div className='books'>
+              <Book 
+              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
+              title='The Dictatorship Syndrome'
+              author='Alaa Al Aswany'
+              year='2019'
+              />
+              <Book 
+              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
+              title='Boikoter Jibon'
+              author='Sami Khan'
+              year='2022'
+              />
+              <Book 
+              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
+              title='Boikoter Jibon'
+              author='Sami Khan'
+              year='2022'
+              />
+              <Book 
+              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
+              title='Boikoter Jibon'
+              author='Sami Khan'
+              year='2022'
+              />
+            </div>
+            
+            <h2 className='group'>Editors Pick</h2>
+            <div className='books'>
+              <Book 
+              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
+              title='The Dictatorship Syndrome'
+              author='Alaa Al Aswany'
+              year='2019'
+              />
+              <Book 
+              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
+              title='Boikoter Jibon'
+              author='Sami Khan'
+              year='2022'
+              />
+              <Book 
+              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
+              title='Boikoter Jibon'
+              author='Sami Khan'
+              year='2022'
+              />
+              <Book 
+              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
+              title='Boikoter Jibon'
+              author='Sami Khan'
+              year='2022'
+              />
+            </div>
+          </div>
           <div className='footer'>footer</div>
         </div>
     </div>
---
+++ client/src/Components/Book/Book.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Book.css';
+
+function Book(props) {
+  return (
+    <div className='wrapper'>
+        <div className='card'>
+            <img src={props.img}></img>
+            <div className='info'>
+                <h1>{props.title}</h1>
+                <p>{props.author}</p>
+                <p>{props.year}</p>
+                <button className='button'>Read Now</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Book
