0164e584332e68a3c62230a4eac19e8d2be62935
Tariquzzaman-faisal <tariquzzaman@iut-dhaka.edu>
2022-08-28 15:11:12 +0600

    working on discover page

---
+++ client/src/pages/Discover.jsx
@@ -0,0 +1,116 @@
+import React from 'react';
+import './Discover.css';
+import * as BsIcons from 'react-icons/bs';
+
+function Discover() {
+  return (
+    <div className='items'>
+        <div className='item'>
+            <i><BsIcons.BsBuilding /></i>
+            <p>Architecture</p>
+        </div>
+        <div className='item'>
+            <i><BsIcons.BsBuilding /></i>
+            <p>Architecture</p>
+        </div>
+        <div className='item'>
+            <i><BsIcons.BsBuilding /></i>
+            <p>Architecture</p>
+        </div>
+
+        <div className='item'>
+            <i><BsIcons.BsBuilding /></i>
+            <p>Architecture</p>
+        </div>
+        <div className='item'>
+            <i><BsIcons.BsBuilding /></i>
+            <p>Architecture</p>
+        </div>
+        <div className='item'>
+            <i><BsIcons.BsBuilding /></i>
+            <p>Architecture</p>
+        </div>
+        
+        <div className='item'>
+            <i><BsIcons.BsBuilding /></i>
+            <p>Architecture</p>
+        </div>
+        <div className='item'>
+            <i><BsIcons.BsBuilding /></i>
+            <p>Architecture</p>
+        </div>
+        <div className='item'>
+            <i><BsIcons.BsBuilding /></i>
+            <p>Architecture</p>
+        </div>
+        
+        <div className='item'>
+            <i><BsIcons.BsBuilding /></i>
+            <p>Architecture</p>
+        </div>
+        <div className='item'>
+            <i><BsIcons.BsBuilding /></i>
+            <p>Architecture</p>
+        </div>
+        <div className='item'>
+            <i><BsIcons.BsBuilding /></i>
+            <p>Architecture</p>
+        </div>
+        
+        <div className='item'>
+            <i><BsIcons.BsBuilding /></i>
+            <p>Architecture</p>
+        </div>
+        <div className='item'>
+            <i><BsIcons.BsBuilding /></i>
+            <p>Architecture</p>
+        </div>
+        <div className='item'>
+            <i><BsIcons.BsBuilding /></i>
+            <p>Architecture</p>
+        </div>
+        
+        <div className='item'>
+            <i><BsIcons.BsBuilding /></i>
+            <p>Architecture</p>
+        </div>
+        <div className='item'>
+            <i><BsIcons.BsBuilding /></i>
+            <p>Architecture</p>
+        </div>
+        <div className='item'>
+            <i><BsIcons.BsBuilding /></i>
+            <p>Architecture</p>
+        </div>
+        
+        <div className='item'>
+            <i><BsIcons.BsBuilding /></i>
+            <p>Architecture</p>
+        </div>
+        <div className='item'>
+            <i><BsIcons.BsBuilding /></i>
+            <p>Architecture</p>
+        </div>
+        <div className='item'>
+            <i><BsIcons.BsBuilding /></i>
+            <p>Architecture</p>
+        </div>
+        
+        <div className='item'>
+            <i><BsIcons.BsBuilding /></i>
+            <p>Architecture</p>
+        </div>
+        <div className='item'>
+            <i><BsIcons.BsBuilding /></i>
+            <p>Architecture</p>
+        </div>
+        <div className='item'>
+            <i><BsIcons.BsBuilding /></i>
+            <p>Architecture</p>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Discover
