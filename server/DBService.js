6a3a5e94de100d6d8a2d4ee9f189890d28093c29
samikhan-cse19 <samikhan@iut-dhaka.edu>
2022-08-28 06:59:08 +0600

    Send all book metadata from server

--- server/DBService.js
+++ server/DBService.js
@@ -21,9 +21,27 @@ class DbService {
             const response = await new Promise((resolve, reject) => {
                 const query = "SELECT * FROM book_info;";
 
-                connection.query(query, (err, results) => {
+                connection.execute(query, (err, results) => {
                     if (err) reject(new Error(err.message));
                     resolve(results);
+                })
+            });
+            // console.log(response);
+            return response;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static async getOneBook(title) {
+        try {
+            const response = await new Promise((resolve, reject) => {
+                const query = "SELECT * FROM `book_info` WHERE `title` = '" + title + "';";
+                connection.query(
+                    query, 
+                    (err, results) => {
+                        if (err) reject(new Error(err.message));
+                        resolve(results);
                 })
             });
             // console.log(response);
