73006839930fbad500867fa03c5f717850e62be0
Tariquzzaman-faisal <tariquzzaman@iut-dhaka.edu>
2022-08-28 16:56:50 +0600

    wishlist and completed done

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,9 @@ import Home from './pages/Home.jsx';
 import Reading from './pages/Reading';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Discover from './pages/Discover.jsx';
+import Wishlist from './pages/Wishlist';
+import Completed from './pages/Completed';
+
 function App() {
   return (
     <BrowserRouter>
@@ -18,7 +21,9 @@ function App() {
             <Routes>
               <Route exact path='/' element={<Home />} />
               <Route exact path='/discover' element={<Discover />} />
-              <Route exact path='/reading' element={<Reading />} /> 
+              <Route exact path='/reading' element={<Reading />} />
+              <Route exact path='/wishlist' element={<Wishlist />} />
+              <Route exact path='/completed' element={<Completed />} />
             </Routes>
           </div>
           <div className='footer'>footer</div>
