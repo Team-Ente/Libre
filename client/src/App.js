992f1715e64033be6ff16433eb98b67b3d4ca79e
Tariquzzaman-faisal <tariquzzaman@iut-dhaka.edu>
2022-08-28 13:03:18 +0600

    react router implemented

--- client/src/App.js
+++ client/src/App.js
@@ -2,130 +2,28 @@ import './App.css';
 import Logo from './Components/Logo/logo';
 import Header from './Components/header/header';
 import Sidenav from './Components/Sidenav/Sidenav';
-import Book from './Components/Book/Book';
-
+import Home from './pages/Home.jsx';
+import Reading from './pages/Reading';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className='App'>
+    <BrowserRouter>
+
+      <div className='App'>
         <div className='container'>
           <div className='logo'><Logo /><Sidenav /></div>
           <div className='header'><Header /></div>
-          <div className='content'>
-            <h2 className='group'>Now Reading</h2>
-            <div className='books'>
-              <Book 
-              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
-              title='The Dictatorship Syndrome'
-              author='Alaa Al Aswany'
-              year='2019'
-              />
-              <Book 
-              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
-              title='Boikoter Jibon'
-              author='Sami Khan'
-              year='2022'
-              />
-              <Book 
-              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
-              title='Boikoter Jibon'
-              author='Sami Khan'
-              year='2022'
-              />
-              <Book 
-              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
-              title='Boikoter Jibon'
-              author='Sami Khan'
-              year='2022'
-              />
-            </div>
-
-            <h2 className='group'>Trending</h2>
-            <div className='books'>
-              <Book 
-              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
-              title='The Dictatorship Syndrome'
-              author='Alaa Al Aswany'
-              year='2019'
-              />
-              <Book 
-              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
-              title='Boikoter Jibon'
-              author='Sami Khan'
-              year='2022'
-              />
-              <Book 
-              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
-              title='Boikoter Jibon'
-              author='Sami Khan'
-              year='2022'
-              />
-              <Book 
-              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
-              title='Boikoter Jibon'
-              author='Sami Khan'
-              year='2022'
-              />
-            </div>
-
-            <h2 className='group'>Recently Added</h2>
-            <div className='books'>
-              <Book 
-              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
-              title='The Dictatorship Syndrome'
-              author='Alaa Al Aswany'
-              year='2019'
-              />
-              <Book 
-              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
-              title='Boikoter Jibon'
-              author='Sami Khan'
-              year='2022'
-              />
-              <Book 
-              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
-              title='Boikoter Jibon'
-              author='Sami Khan'
-              year='2022'
-              />
-              <Book 
-              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
-              title='Boikoter Jibon'
-              author='Sami Khan'
-              year='2022'
-              />
-            </div>
-            
-            <h2 className='group'>Editors Pick</h2>
-            <div className='books'>
-              <Book 
-              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
-              title='The Dictatorship Syndrome'
-              author='Alaa Al Aswany'
-              year='2019'
-              />
-              <Book 
-              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
-              title='Boikoter Jibon'
-              author='Sami Khan'
-              year='2022'
-              />
-              <Book 
-              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
-              title='Boikoter Jibon'
-              author='Sami Khan'
-              year='2022'
-              />
-              <Book 
-              img="https://images.unsplash.com/photo-1477666250292-1419fac4c25c?auto=format&fit=crop&w=667&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"
-              title='Boikoter Jibon'
-              author='Sami Khan'
-              year='2022'
-              />
-            </div>
+          <div className='content'>          
+            <Routes>
+              <Route exact path='/' element={<Home />} />
+              <Route exact path='/reading' element={<Reading />} /> 
+            </Routes>
           </div>
           <div className='footer'>footer</div>
         </div>
     </div>
+    </BrowserRouter>
+    
   );
 }
 
