8ff3e2de14c8da7dd75e45a5b11560fe8b2239df
Tariquzzaman-faisal <tariquzzaman@iut-dhaka.edu>
2022-08-28 16:48:59 +0600

    fixed layout

--- client/src/pages/Discover.jsx
+++ client/src/pages/Discover.jsx
@@ -1,114 +1,124 @@
 import React from 'react';
 import './Discover.css';
 import * as BsIcons from 'react-icons/bs';
+import * as FaIcons from 'react-icons/fa';
+import * as BiIcons from 'react-icons/bi';
+import * as SiIcons from 'react-icons/si';
+import * as AiIcons from 'react-icons/ai';
+import * as VsIcons from 'react-icons/vsc';
+import * as GrIcons from 'react-icons/gr';
+import * as MdIcons from 'react-icons/md';
+import * as IoIcons from 'react-icons/io5';
 
 function Discover() {
   return (
-    <div className='items'>
+    <div>
+      <h2>Explore</h2>
+      <div className='items'>
         <div className='item'>
             <i><BsIcons.BsBuilding /></i>
             <p>Architecture</p>
         </div>
         <div className='item'>
-            <i><BsIcons.BsBuilding /></i>
-            <p>Architecture</p>
+            <i><BiIcons.BiPaint /></i>
+            <p>Art</p>
         </div>
         <div className='item'>
-            <i><BsIcons.BsBuilding /></i>
-            <p>Architecture</p>
+            <i><BiIcons.BiDna /></i>
+            <p>Biology</p>
         </div>
 
         <div className='item'>
-            <i><BsIcons.BsBuilding /></i>
-            <p>Architecture</p>
+            <i><BsIcons.BsBriefcase /></i>
+            <p>Business</p>
         </div>
         <div className='item'>
-            <i><BsIcons.BsBuilding /></i>
-            <p>Architecture</p>
+            <i><BsIcons.BsCodeSquare /></i>
+            <p>Computer Science</p>
         </div>
         <div className='item'>
-            <i><BsIcons.BsBuilding /></i>
-            <p>Architecture</p>
+            <i><SiIcons.SiAltiumdesigner /></i>
+            <p>Design</p>
+        </div>
+        
+        <div className='item'>
+            <i><AiIcons.AiOutlineLineChart /></i>
+            <p>Economic</p>
+        </div>
+        <div className='item'>
+            <i><VsIcons.VscMortarBoard /></i>
+            <p>Education</p>
+        </div>
+        <div className='item'>
+            <i><GrIcons.GrGlobe /></i>
+            <p>History</p>
+        </div>
+        
+        <div className='item'>
+            <i><MdIcons.MdTranslate /></i>
+            <p>Language</p>
+        </div>
+        <div className='item'>
+            <i><FaIcons.FaBalanceScaleLeft /></i>
+            <p>Law</p>
+        </div>
+        <div className='item'>
+            <i><IoIcons.IoBookOutline /></i>
+            <p>Literature</p>
         </div>
         
         <div className='item'>
             <i><BsIcons.BsBuilding /></i>
-            <p>Architecture</p>
+            <p>Mathematics</p>
         </div>
         <div className='item'>
             <i><BsIcons.BsBuilding /></i>
-            <p>Architecture</p>
+            <p>Performing Arts</p>
         </div>
         <div className='item'>
             <i><BsIcons.BsBuilding /></i>
-            <p>Architecture</p>
+            <p>Medicine</p>
         </div>
         
         <div className='item'>
             <i><BsIcons.BsBuilding /></i>
-            <p>Architecture</p>
+            <p>Self Development</p>
         </div>
         <div className='item'>
             <i><BsIcons.BsBuilding /></i>
-            <p>Architecture</p>
+            <p>Philospphy</p>
         </div>
         <div className='item'>
             <i><BsIcons.BsBuilding /></i>
-            <p>Architecture</p>
+            <p>Physical Sciences</p>
         </div>
         
         <div className='item'>
             <i><BsIcons.BsBuilding /></i>
-            <p>Architecture</p>
+            <p>Politics</p>
         </div>
         <div className='item'>
             <i><BsIcons.BsBuilding /></i>
-            <p>Architecture</p>
+            <p>Psychology</p>
         </div>
         <div className='item'>
             <i><BsIcons.BsBuilding /></i>
-            <p>Architecture</p>
+            <p>Social Sciences</p>
         </div>
         
         <div className='item'>
             <i><BsIcons.BsBuilding /></i>
-            <p>Architecture</p>
+            <p>Study Aids</p>
         </div>
         <div className='item'>
             <i><BsIcons.BsBuilding /></i>
-            <p>Architecture</p>
+            <p>Technology</p>
         </div>
         <div className='item'>
             <i><BsIcons.BsBuilding /></i>
-            <p>Architecture</p>
+            <p>Religion</p>
         </div>
-        
-        <div className='item'>
-            <i><BsIcons.BsBuilding /></i>
-            <p>Architecture</p>
-        </div>
-        <div className='item'>
-            <i><BsIcons.BsBuilding /></i>
-            <p>Architecture</p>
-        </div>
-        <div className='item'>
-            <i><BsIcons.BsBuilding /></i>
-            <p>Architecture</p>
-        </div>
-        
-        <div className='item'>
-            <i><BsIcons.BsBuilding /></i>
-            <p>Architecture</p>
-        </div>
-        <div className='item'>
-            <i><BsIcons.BsBuilding /></i>
-            <p>Architecture</p>
-        </div>
-        <div className='item'>
-            <i><BsIcons.BsBuilding /></i>
-            <p>Architecture</p>
-        </div>
-        
+      </div>  
     </div>
   )
 }
