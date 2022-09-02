dd48c54786afe3c640d319a555a55a312aae4748
Tariquzzaman-faisal <tariquzzaman@iut-dhaka.edu>
2022-09-02 08:55:09 +0600

    signup and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,43 @@
-import './App.css';
+import React from 'react'
+import Discover from './pages/Landing/Discover';
+import Wishlist from './pages/Landing/Wishlist';
+import Completed from './pages/Landing/Completed';
 import Logo from './Components/Logo/logo';
 import Header from './Components/header/header';
 import Sidenav from './Components/Sidenav/Sidenav';
 import Home from './pages/Landing/Home';
 import Reading from './pages/Landing/Reading';
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import Discover from './pages/Landing/Discover';
-import Wishlist from './pages/Landing/Wishlist';
-import Completed from './pages/Landing/Completed';
-import login from './pages/login&signup/login';
-import signup from './pages/login&signup/signup';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import "./App.css";
 import { AnimatePresence } from 'framer-motion';
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
 function App() {
-
   const location = useLocation();
   return (
-    
-      <div className='App'>
+    <div>
+
         <div className='container'>
           <div className='logo'><Logo /><Sidenav /></div>
           <div className='header'><Header /></div>
           <div className='content'>
             <AnimatePresence exitBeforeEnter>    
               <Routes key={location.pathname} location={location}>
-                <Route exact path='/' element={<Home />} />
+                <Route exact path='/home' element={<Home />} />
                 <Route exact path='/discover' element={<Discover />} />
                 <Route exact path='/reading' element={<Reading />} />
                 <Route exact path='/wishlist' element={<Wishlist />} />
+                <Route exact path='/completed' element={<Completed />} />
+                <Route exact path='/login' element={<Login />} />
+                <Route exact path='/signup' element={<Signup />} />
               </Routes>
             </AnimatePresence>
           </div>
           {/* <div className='footer'>footer</div> */}
         </div>
     </div>
-    
-  );
+  )
 }
 
-export default App;
+export default App
