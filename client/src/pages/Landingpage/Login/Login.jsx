1a0fb72b22518ba0bc875bb411e41bfd787d6781
Tariquzzaman-faisal <tariquzzaman@iut-dhaka.edu>
2022-09-10 13:12:30 +0600

    post action added

--- client/src/pages/Landingpage/Login/Login.jsx
+++ client/src/pages/Landingpage/Login/Login.jsx
@@ -10,21 +10,20 @@ function Login() {
     <div className='Login'>
         <div className="center">
         <Link to='/'><h1 className='icon'><i className='libre-icon'><IcoIcons.ImBooks /></i>Login</h1></Link>
-        <form method="post">
+        <form method="post" action='/login'>
             <div className="txt_field">
-            <input type="text" required />
-            <span></span>
-            <label>Username or Email</label>
+              <input type="text" name='username_email' required />
+              <span></span>
+              <label>Username or Email</label>
             </div>
             <div className="txt_field">
-            <input type="password" required />
-            <span></span>
-            <label>Password</label>
+              <input type="password" name='password' required />
+              <span></span>
+              <label>Password</label>
             </div>
-            <div className="pass">Forgot Password?</div>
-            <input type="submit" value="Login" />
-            <div className="signup_link">
-            Not a member? <Link to="/signup">Signup</Link>
+              <div className="pass">Forgot Password?</div>
+              <input type="submit" name='login_btn' value="Login" />
+              <div className="signup_link">Not a member? <Link to="/signup">Signup</Link>
             </div>
         </form>
         </div>
