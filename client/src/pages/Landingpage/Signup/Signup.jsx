e457094999b51c491eda9f500c62d1ab0b669c98
Tariquzzaman-faisal <tariquzzaman@iut-dhaka.edu>
2022-09-10 14:57:13 +0600

    email validation check

--- client/src/pages/Landingpage/Signup/Signup.jsx
+++ client/src/pages/Landingpage/Signup/Signup.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useState } from 'react';
 import * as IcoIcons from 'react-icons/im'; 
 import { Link } from 'react-router-dom';
 import "./Signup.css";
 import Animation from '../Animation';
 function Signup() {
+  const regex = /\S+@\S+\.\S+/;
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState('');
+  const checkEmail = (e) =>{
+    setEmail(e.target.value);
+    if(regex.test(email)===false) {
+      setError('Please enter valid Email')
+    } else {
+      setError('');
+      return true;
+    }
+  }
   return (
     <Animation>
 
@@ -12,14 +25,28 @@ function Signup() {
         <Link to='/'><h1 className='icon'><i className='libre-icon'><IcoIcons.ImBooks /></i>Sign up</h1></Link>
         <form method="post" action='/signup_user'>
             <div className="txt_field">
-              <input type="text" name='username' required />
+              <input type="text" name='handle' required />
               <span></span>
               <label>Username</label>
             </div>
             <div className="txt_field">
-              <input type="text" name='email' required />
+              <input type="text" name='email' onChange={checkEmail} required />
               <span></span>
               <label>Email</label>
+            </div>
+            <p className='email-error'>{error}</p>
+
+            <div className="txt_field">
+              <input type="text" name='firstName' required />
+              <span></span>
+              <label>First Name</label>
+            </div>
+
+
+            <div className="txt_field">
+              <input type="text" name='lastName' required />
+              <span></span>
+              <label>Last Name</label>
             </div>
             <div className="txt_field">
               <input type="password" name='password' required />
