5fc797fc1f6ba597957622c8c3ae486efb6efa69
Tariquzzaman-faisal <tariquzzaman@iut-dhaka.edu>
2022-09-10 16:11:37 +0600

    email and password validation done

--- client/src/pages/Landingpage/Login/Login.jsx
+++ client/src/pages/Landingpage/Login/Login.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useState } from 'react';
 import * as IcoIcons from 'react-icons/im'; 
 import { Link } from 'react-router-dom';
 import "./Login.css";
 import Animation from '../Animation';
 function Login() {
+  const passRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  const [pass, setPass] = useState('');
+  const [pvalid, setPvalid] = useState('');
+
+  const checkPass = (e) => {
+    console.log(e.target.value);
+    setPass(e.target.value);
+    if(passRegex.test(pass)===false) {
+      setPvalid('Please enter a valid password');
+    } else {
+      setPvalid('');
+      return true;
+    }
+  }
   return (
     <Animation>
 
@@ -17,10 +32,12 @@ function Login() {
               <label>Username or Email</label>
             </div>
             <div className="txt_field">
-              <input type="password" name='password' required />
+              <input type="password" name='password' onChange={checkPass} required />
               <span></span>
               <label>Password</label>
             </div>
+            <p className='email-error'>{pvalid}</p>
+
               <div className="pass">Forgot Password?</div>
               <input type="submit" name='login_btn' value="Login" />
               <div className="signup_link">Not a member? <Link to="/signup">Signup</Link>
