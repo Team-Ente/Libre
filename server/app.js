1d3ea1dd5dd23de3e4b73e26b929af7cc9f0e699
samikhan-cse19 <samikhan@iut-dhaka.edu>
2022-09-22 07:08:01 +0600

    file upload incomplete

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 import Express from "express";
 import cookieParser from "cookie-parser";
+import multer from "multer";
 
 import { register, login, verifyToken, logout } from "./middleware/Auth.js";
 import {query} from "./middleware/BookQuery.js"
 import { getContents } from "./middleware/BookRead.js";
 import { bypassCORS } from "./middleware/Setup.js";
+import { uploadBook } from "./middleware/Admin.js";
+
 
 const app = Express();
 
@@ -23,6 +26,11 @@ app.post('/register', bypassCORS, register);
 app.post('/login', bypassCORS, verifyToken, login);
 
 app.post('/logout', bypassCORS, verifyToken, logout);
+
+// book upload
+app.post('/upload', uploadBook, (req, res) => {
+    res.send("success");
+});
 
 
 app.listen(3050, "localhost", () => {
