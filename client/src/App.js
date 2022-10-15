3d1a6e4087a3366ec8e8b979df1b28909d65f313
samikhan-cse19 <samikhan@iut-dhaka.edu>
2022-10-15 13:48:52 +0600

    cleaned up code

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,17 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Homepage from './pages/Homepage/Homepage';
 import Landingpage from './pages/Landingpage/Landingpage';
 import Reader from './pages/Reader/Reader';
-import { Route, Routes, useLocation } from 'react-router-dom';
-import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
 function App() {
 
-  const [toc, setToc] = useState([]);
-  const [pages, setPages] = useState([]);
-
-  const [book, setBook] = useState("LN_test_1");
-  const [chapter, setChapter] = useState("chapter002");
-
-  // const setReaderBook = async (newBook, newChapter) => {
-  //   setBook(newBook);
-  //   if(chapter) setChapter(newChapter);
-  //   fetch("http://localhost:3050/read?book="+book, {
-  //       mode: "cors",
-  //       credentials: "include"
-  //   }).then((result) => {
-  //       result.json().then((jsonResult) => {
-  //           setToc(jsonResult.contents);
-  //           setPages(jsonResult.pages)
-  //       });   
-  //   }, (reason) => {
-  //       console.log(reason);
-  //   });
-  // }
-
-  // useEffect(() => {
-  //   // check logged in user
-
-  //   setReaderBook(book, chapter);
-  // });
 
   return (
       <div className='App'>
         <Routes>
           <Route path='/*' element={<Landingpage />} />
           <Route path='/home/*' element={<Homepage/>} />
-          <Route path='/reader' element={<Reader state={
-            {
-              book: book,
-              chapter: chapter, 
-              toc: toc, 
-              pages: pages
-            }
-          }
-          />} />
+          <Route path='/reader' element={<Reader/>} />
         </Routes>
       </div>
   );
