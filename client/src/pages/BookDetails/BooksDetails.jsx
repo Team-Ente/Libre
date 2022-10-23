78f16fd688dc120900cc9b774dfd2be01bd7eaf1
Tariquzzaman-faisal <tariquzzaman@iut-dhaka.edu>
2022-10-23 13:27:27 +0600

    book details updated

--- client/src/pages/BookDetails/BooksDetails.jsx
+++ client/src/pages/BookDetails/BooksDetails.jsx
@@ -14,17 +14,20 @@ function BooksDetails() {
         </div>
         <div className='description'>
 
-          <div className='title'>The Horni Stori of Sami</div>        
-          <div className='author'>Tariquzzaman Faisal</div>
+          <div className='title'>1984</div>        
+          <div className='author'>George Orwell</div>
           <div className='summary'>
-          This book is very good book. Many Many information many action scene
-          Very hardcode action. Blood Sport involved. In cases individuals are beaten till they scream God's Name<br/>
-          Includes mental abuse by a friend who are trying to teach a lesson to their classmate. 
-          The story revolves around a boy sami who has recently discovered adult manga. While reading those exotic manga, one day he gets caught in the school library. He is then soon sent to his parents. Being confined in four walls, Sami becomes desperate for quality adult manga. So he contacts his local biri seller, Saikat to help. Seeing an opportunity, Saikat opens a startup where his main products are adult managa with a hole inside the book containing ganja. He hopes to become biribara within the next 5 years. Sami eventually manages to escape his house when a misfired missile by russia hits his bedroom wall, destroying it completely.
-          <br/>
-          Upon his release, Sami becomes an avid hentai reader. He becomes a leading Gunda of BiriBaba Gang after being lured by saikat's manga collection. Post world war 3, the world is now in chaos and gangs are trying to take over the city. The endangered city now requires a hero who can save them and bring order to this chaos. Learn how a simple boy becomes "Sami Man" to declare war against evil. Can "Sami man" save us using his horni powers? Only time will tell.
+          George Orwell's nineteen Eighty-Four is one of the most definitive texts of modern literature. Set in Oceania, one of the three inter-continental superstate that divided the world among themselves after a global war, Orwell's masterful critique of the political structures of the time, works itself out through the story of Winston Smith, a man caught in the webs of a dystopian future, and his clandestine love affair with Julia, a young woman he meets during the course of his work for the government. As much as it is an entertaining read, nineteen Eighty-Four is also a brilliant, and more importantly, a timeless satirical attack on the social and political structures of the world.
+          </div>
         </div>
-        </div>
+      </div>
+      <div className='additional-info'>
+        <p className='language'>Language: English</p>
+        <p className='publisher'>Publisher: GroMedia</p>
+        <p className='topic'>Topic: Literature</p>
+        <p className='format'>Format: ePub & PDF</p>
+        <p className='year'>Publishing Year: 2021</p>
+        <p className='ISBN'>ISBN: 9782291963134</p>
       </div>
     </div>
   )
