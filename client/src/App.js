bbac25dd2ec5ead4e5d957ad02720f9eb95699a3
Tariquzzaman-faisal <tariquzzaman@iut-dhaka.edu>
2022-10-23 10:28:13 +0600

    /error leads to error page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Homepage from './pages/Homepage/Homepage';
 import Landingpage from './pages/Landingpage/Landingpage';
 import Reader from './pages/Reader/Reader';
+import ErrorPage from './pages/Error Page/ErrorPage';
 import { Route, Routes } from 'react-router-dom';
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
           <Route path='/*' element={<Landingpage />} />
           <Route path='/home/*' element={<Homepage/>} />
           <Route path='/reader' element={<Reader/>} />
+          <Route path='/Error' element={<ErrorPage />} />
         </Routes>
       </div>
   );
