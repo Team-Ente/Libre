799b5a5bc8ef61584e0dd7762975e9e8a1c7f2bb
Tariquzzaman-faisal <tariquzzaman@iut-dhaka.edu>
2022-10-23 13:12:50 +0600

    separated error pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React from 'react'
 import Homepage from './pages/Homepage/Homepage';
 import Landingpage from './pages/Landingpage/Landingpage';
 import Reader from './pages/Reader/Reader';
-import ErrorPage from './pages/Error Page/ErrorPage';
+import Error401 from './pages/Error Page/Error401';
+import Error404 from './pages/Error Page/Error404';
 import BooksDetails from './pages/BookDetails/BooksDetails';
 import { Route, Routes } from 'react-router-dom';
 function App() {
@@ -14,7 +15,8 @@ function App() {
           <Route path='/*' element={<Landingpage />} />
           <Route path='/home/*' element={<Homepage/>} />
           <Route path='/reader' element={<Reader/>} />
-          <Route path='/Error' element={<ErrorPage />} />
+          <Route path='/error404' element={<Error404 />} />
+          <Route path='/error401' element={<Error401 />} />
           <Route path='/bookdetails' element={<BooksDetails />} />
         </Routes>
       </div>
