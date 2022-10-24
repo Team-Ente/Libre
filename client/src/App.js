c9bbbd696c765289389f1a294d2c47b763216f7d
samikhan-cse19 <samikhan@iut-dhaka.edu>
2022-10-24 07:29:10 +0600

    error pages routed

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Homepage from './pages/Homepage/Homepage';
 import Landingpage from './pages/Landingpage/Landingpage';
 import Reader from './pages/Reader/Reader';
+import Login from './pages/Landingpage/Login/Login';
+import Signup from './pages/Landingpage/Signup/Signup';
 import Error401 from './pages/Error Page/Error401';
 import Error404 from './pages/Error Page/Error404';
 import BooksDetails from './pages/BookDetails/BooksDetails';
@@ -12,12 +14,14 @@ function App() {
   return (
       <div className='App'>
         <Routes>
-          <Route path='/*' element={<Landingpage />} />
+          <Route path='/' element={<Landingpage />} />
+          <Route path ='/login' element={<Login />} />
+          <Route path ='/signup' element={<Signup />} />
           <Route path='/home/*' element={<Homepage/>} />
           <Route path='/reader' element={<Reader/>} />
-          <Route path='/error404' element={<Error404 />} />
-          <Route path='/error401' element={<Error401 />} />
+          <Route path='/unauthorized' element={<Error401 />} />
           <Route path='/book' element={<BooksDetails />} />
+          <Route path='*' element={<Error404 />} />
         </Routes>
       </div>
   );
