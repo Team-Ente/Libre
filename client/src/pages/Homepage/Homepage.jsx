0fad9f8d1e4588d75e61b58a48ae59afa61a0f2b
samikhan-cse19 <samikhan@iut-dhaka.edu>
2022-10-24 07:34:11 +0600

    login bypassed for already logged in user

--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -6,8 +6,6 @@ import Completed from './Completed/Completed';
 import Reading from './Reading/Reading';
 import Home from './Home/Home';
 import AdvancedSearch from '../AdvancedSearch/AdvancedSearch';
-import Error404 from '../Error Page/Error404';
-import Reader from '../Reader/Reader';
 import Logo from '../../Components/Logo/Logo';
 import Header from '../../Components/Header/Header';
 import Sidenav from '../../Components/Sidenav/Sidenav';
