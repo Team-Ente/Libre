9b4934002b0e486f64e87dafa54541b7284c99ea
samikhan-cse19 <samikhan@iut-dhaka.edu>
2022-10-24 07:55:13 +0600

    routed Book Details page

--- client/src/pages/BookDetails/BooksDetails.jsx
+++ client/src/pages/BookDetails/BooksDetails.jsx
@@ -1,21 +1,53 @@
 import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import './BookDetails.css';
 
 function BooksDetails() {
+
+  const {state} = useLocation();
+  const data = state;
+
+  const navigate = useNavigate();
+  const readBook = async () => {
+    
+    fetch("http://localhost:3050/read?book="+data.id, {
+        mode: "cors",
+        credentials: "include"
+    }).then((result) => {
+        result.json().then((jsonResult) => {
+            const toc = jsonResult.contents;
+            const pages = jsonResult.pages;
+            
+            navigate('/reader', {
+              state: {
+                id: data.id,
+                title: data.title,
+                // chapter: chapter, 
+                toc: toc, 
+                pages: pages
+              }
+            });
+
+        });   
+    }, (reason) => {
+        console.log(reason);
+    });
+
+  };
   return (
     <div className='bookdetails'>
       <div className='book-card-grid'>
         <div className='bookimg'>
-        <img src={require('./35356.jpg')} style={{height: 500}}/>
+        <img src={data.img} alt={data.title} style={{height: 500}}/>
         <div className='controlbtns'>
-          <button className='details-control-btn'>Read Now</button>
+          <button className='details-control-btn' onClick={readBook}>Read Now</button>
           <button className='details-control-btn'>Add to wishlist</button>
         </div>
         </div>
         <div className='description'>
 
-          <div className='title'>1984</div>        
-          <div className='author'>George Orwell</div>
+          <div className='title'>{data.title}</div>        
+          <div className='author'>{data.author}</div>
           <div className='summary'>
           George Orwell's nineteen Eighty-Four is one of the most definitive texts of modern literature. Set in Oceania, one of the three inter-continental superstate that divided the world among themselves after a global war, Orwell's masterful critique of the political structures of the time, works itself out through the story of Winston Smith, a man caught in the webs of a dystopian future, and his clandestine love affair with Julia, a young woman he meets during the course of his work for the government. As much as it is an entertaining read, nineteen Eighty-Four is also a brilliant, and more importantly, a timeless satirical attack on the social and political structures of the world.
           </div>
