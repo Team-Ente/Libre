62bd3088fa187b26fc6cd1aec82cbf37d75ff3bd
Tariquzzaman-faisal <tariquzzaman@iut-dhaka.edu>
2022-10-29 18:41:39 +0600

    upload form

--- client/src/pages/BookUpload/BookUpload.jsx
+++ client/src/pages/BookUpload/BookUpload.jsx
@@ -1,69 +1,12 @@
 import React from 'react'
 import './BookUpload.css'
 import * as AiIcons from 'react-icons/ai'
+import TopBar from '../../Components/TopBar/TopBar'
 function BookUpload() {
-  const form = document.querySelector("form"),
-fileInput = document.querySelector(".file-input"),
-progressArea = document.querySelector(".progress-area"),
-uploadedArea = document.querySelector(".uploaded-area");
-form.addEventListener("click", () =>{
-  fileInput.click();
-});
-fileInput.onchange = ({target})=>{
-  let file = target.files[0];
-  if(file){
-    let fileName = file.name;
-    if(fileName.length >= 12){
-      let splitName = fileName.split('.');
-      fileName = splitName[0].substring(0, 13) + "... ." + splitName[1];
-    }
-    uploadFile(fileName);
-  }
-}
-function uploadFile(name){
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", "php/upload.php");
-  xhr.upload.addEventListener("progress", ({loaded, total}) =>{
-    let fileLoaded = Math.floor((loaded / total) * 100);
-    let fileTotal = Math.floor(total / 1000);
-    let fileSize;
-    (fileTotal < 1024) ? fileSize = fileTotal + " KB" : fileSize = (loaded / (1024*1024)).toFixed(2) + " MB";
-    let progressHTML = `<li class="row">
-                          <i class="fas fa-file-alt"></i>
-                          <div class="content">
-                            <div class="details">
-                              <span class="name">${name} • Uploading</span>
-                              <span class="percent">${fileLoaded}%</span>
-                            </div>
-                            <div class="progress-bar">
-                              <div class="progress" style="width: ${fileLoaded}%"></div>
-                            </div>
-                          </div>
-                        </li>`;
-    uploadedArea.classList.add("onprogress");
-    progressArea.innerHTML = progressHTML;
-    if(loaded == total){
-      progressArea.innerHTML = "";
-      let uploadedHTML = `<li class="row">
-                            <div class="content upload">
-                              <i class="fas fa-file-alt"></i>
-                              <div class="details">
-                                <span class="name">${name} • Uploaded</span>
-                                <span class="size">${fileSize}</span>
-                              </div>
-                            </div>
-                            <i class="fas fa-check"></i>
-                          </li>`;
-      uploadedArea.classList.remove("onprogress");
-      uploadedArea.insertAdjacentHTML("afterbegin", uploadedHTML);
-    }
-  });
-  let data = new FormData(form);
-  xhr.send(data);
-}
   return (
+    <div>
+    <TopBar />
     <div className='mainbody'>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
     <div className='container-main'>
     <div className='titleadd'>Add New Book</div>
         <form>
@@ -125,15 +68,11 @@ function uploadFile(name){
                 <textarea required />
             </div>
 
-            <div class="wrapper">
-                <input className="file-input" type={'file'} name="file" hidden/>
-                <i class="fas fa-cloud-upload-alt"></i>
-                <p>Browse File to Upload</p>
-              <section class="progress-area"></section>
-              <section class="uploaded-area"></section>
-            </div>
+            <input className='addfile' type={"file"} />
+            <input className='submitform' type={"submit"} value="submit" />  
           </div>
         </form>
+    </div>
     </div>
     </div>
   )
