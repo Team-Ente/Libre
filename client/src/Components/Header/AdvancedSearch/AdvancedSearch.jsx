f91b7a294c1061a1ecec7ae6ba4438cc56135f58
Tariquzzaman-faisal <tariquzzaman@iut-dhaka.edu>
2022-10-29 23:50:25 +0600

    search completely restructured

--- client/src/Components/Header/AdvancedSearch/AdvancedSearch.jsx
+++ client/src/Components/Header/AdvancedSearch/AdvancedSearch.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './AdvancedSearch.css';
-import DropdownSearch from '../../Components/DropdownSearch/DropdownSearch';
+import DropdownSearch from './DropdownSearch/DropdownSearch';
 
 const language = [
   { label: 'Bengali', value: 'Bengali' },
   { label: 'English', value: 'English' },
 ];
 
-const publication_date = [
-  { label: '2019', value: '2019' },
-  { label: '2020', value: '2020' },
-  { label: '2021', value: '2021' },
-  { label: '2022', value: '2022' },
+const edition = [
+  { label: '1', value: '1' },
+  { label: '2', value: '2' },
+  { label: '3', value: '3' },
+  { label: '4', value: '4' },
 ];
 
 
@@ -44,7 +44,7 @@ function AdvancedSearch() {
           <i><DropdownSearch dat={author} plc="Author" mul={true}/></i>
           <i><DropdownSearch dat={topic} plc="Topic" mul={true}/></i>
           <i><DropdownSearch dat={publisher} plc="Publisher" mul={false}/></i>
-          <i><DropdownSearch dat={publication_date} plc="Publication Date" mul={false}/></i>
+          <i><DropdownSearch dat={edition} plc="Edition" mul={false}/></i>
         </div>
     </div>
   )
