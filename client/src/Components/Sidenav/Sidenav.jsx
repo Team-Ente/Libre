83f096434f593a224b3369a691866050810c16d9
Tariquzzaman-faisal <tariquzzaman@iut-dhaka.edu>
2022-11-04 18:45:50 +0600

    hover fixed

--- client/src/Components/Sidenav/Sidenav.jsx
+++ client/src/Components/Sidenav/Sidenav.jsx
@@ -17,7 +17,7 @@ function Sidenav() {
             <li><Link to='/home/reading'><i><AiIcons.AiFillEye /></i><p>Reading</p></Link></li>
             <li><Link to='/home/wishlist'><i><BsIcons.BsFillBookmarkFill /></i><p>Wishlist</p></Link></li>
             <li><Link to='/home/completed'><i><AiIcons.AiFillCheckCircle /></i><p>Completed</p></Link></li>
-            <div className='activebox'></div>
+            {/* <div className='activebox'></div> */}
         </ul>
     </div>
     </div>
