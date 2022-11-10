4d8deedc961873549dee310e0d4b03c4327266eb
samikhan-cse19 <samikhan@iut-dhaka.edu>
2022-11-10 19:55:09 +0600

    add book partial

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 import Express from "express";
 import cookieParser from "cookie-parser";
-import multer from "multer";
+import fileupload from "express-fileupload";
 import * as dotenv from "dotenv";
 dotenv.config();
 
@@ -11,10 +11,17 @@ import { bypassCORS } from "./middleware/Setup.js";
 import { uploadBook } from "./middleware/Admin.js";
 
 
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { addNewBook } from "./controllers/Books.js";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
 const app = Express();
 
 app.use(Express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(fileupload());
 
 // book query
 app.get("/books/:qType", bypassCORS, verifyToken, query);
@@ -30,8 +37,25 @@ app.post('/login', bypassCORS, verifyToken, login);
 app.post('/logout', bypassCORS, verifyToken, logout);
 
 // book upload
-app.post('/upload', uploadBook, (req, res) => {
-    res.send("success");
+app.post('/upload', async (req, res) => {
+    if(req.files === null) {
+        return res.status(400).json({ msg: "No file uploaded" });
+    }
+
+
+    // check for duplicate in database
+    // const response = await addNewBook(req.body);
+    // console.log(response);
+
+    const file = req.files.ebook;
+    file.mv(`${__dirname}/files/${file.name}`, err => {
+        if(err) {
+            console.log(error);
+            return res.status(500).send(error);
+        }
+
+        res.json("success");
+    })
 });
 
 
