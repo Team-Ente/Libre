39e05fbc4e6a65dfcdc5b265faa91816640e26be
Tariquzzaman-faisal <tariquzzaman@iut-dhaka.edu>
2022-11-12 18:55:04 +0600

    query route updated

--- client/src/pages/Homepage/SearchResult/Searchresult.jsx
+++ client/src/pages/Homepage/SearchResult/Searchresult.jsx
@@ -2,6 +2,10 @@ import React, {useState, useEffect} from 'react';
 import {getBook} from '../../../Components/Book/Book';
 import './SearchResult.css';
 import Animation from '../Animation';
+import DefaultTopbar from '../../../Components/DefaultTopbar/DefaultTopbar';
+import Sidenav from '../../../Components/Sidenav/Sidenav';
+import Logo from '../../../Components/Logo/Logo';
+import Header from '../../../Components/Header/Header';
 function SearchResult(props) {
 
     const [readingList, setReadingList] = useState([]);
@@ -58,14 +62,21 @@ function SearchResult(props) {
 
     return (
     <Animation>
+    <div className='searchbookgrid'>
         <div>
-            <h2> Found {readingList.length} books</h2> 
+            <div className='logo'><Logo /><Sidenav /></div>
+        </div>
+        <div>
+            <div className='header'><Header /></div>
+            <div className='sidenav-content'></div>
+            <h2 className='searchcounter'> Found {readingList.length} books</h2> 
             {/* put search query backend here, i pasted template code from Home.jsx */}
             {/* http://localhost:3000/home/query is the endpoint */}
             <div className='books'>
                 {readingList.map(getBook)}
             </div>
         </div>
+    </div>
     </Animation>
     
     )
